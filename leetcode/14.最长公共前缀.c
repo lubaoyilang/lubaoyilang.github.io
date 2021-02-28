fd4d968ff97eeb515e8d7b3e7dc1f141efa83871
luyuda <luyuda@keep.com>
2021-03-01 01:09:01 +0800

    11

--- leetcode/14.最长公共前缀.c
+++ leetcode/14.最长公共前缀.c
@@ -48,11 +48,127 @@
 // @lc code=start
 
 
+#include<string.h>
+#include<stdio.h>
+#include <stdlib.h>
 
 
 char * longestCommonPrefix(char ** strs, int strsSize){
         
+       if (strsSize == 0) return "";
+       if (strsSize == 1) return strs[0];
+       
 
+       size_t smallLen = 0;
+        for (int i = 0; i < strsSize; i++)
+        {
+            char * list = strs[i];
+            if (strlen(list)==0)
+            {
+                return "";
+            }
+            
+            if (smallLen!=0)
+            {
+                if (smallLen> strlen(list))
+                {
+                    smallLen = strlen(list);
+                }
+                continue;
+            }else{
+                smallLen = strlen(list);
+            }
+        }
+
+        if (smallLen == 0)
+        {
+            return "";
+        }
+
+        char * prefix = (char*)malloc(sizeof(char)*smallLen+1);
+        memset(prefix,0,sizeof(char)*smallLen+1);
+
+        
+        for (int j = 0; j < smallLen; j++)
+        {
+            char temp = '\0';
+            for (int i = 0; i < strsSize; i++)
+            {
+                char *obj = strs[i];
+
+                if (strlen(obj)==0)
+                {
+                    continue;
+                }
+                
+                
+
+                if (temp == '\0')
+                {
+                    temp = obj[j];
+                    continue;
+                }else{
+                    if (temp == obj[j])
+                    {
+                        continue;
+                    }else{
+                        temp = '\0';
+                        break;
+                    }
+                    
+                }
+
+            }
+            if (temp != '\0')
+            {
+                prefix[j] = temp;
+            }else{
+                return prefix;
+            }
+            
+                
+        }
+
+        return prefix;
+        
+        
+        
 }
+
 // @lc code=end
 
+
+
+/*
+
+int main(int argc, const char * argv[]) {
+    
+    char* a1 = (char*)malloc(sizeof(char)*3);
+//    char* a2 = (char*)malloc(sizeof(char)*1);
+    char* a3 = (char*)malloc(sizeof(char)*1);
+
+    a1 = "";
+
+//    a1[0] = 'a';
+//    a1[1] = 'b';
+//    a1[2] = 'b';
+
+//    a2[0] = 'a';
+//    a2[1] = 'b';
+//    a2[2] = 'x';
+
+    a3[0] = 'b';
+
+    char** p = (char**)malloc(sizeof(char*)*2);
+
+    p[0] = a1;
+    p[1] = a3;
+
+    char *ret = longestCommonPrefix(p,2);
+    printf("%s",ret);
+    return 0;
+}
+
+
+
+*/
